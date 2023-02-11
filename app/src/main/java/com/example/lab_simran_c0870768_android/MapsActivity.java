428946a2b73c4a739bb18fb8ccb93ab6929a3408
simran mehra <simranmehra.iv@gmail.com>
2023-02-10 21:39:17 -0500

    Added dummy data in the list ans search implemented

--- app/src/main/java/com/example/lab_simran_c0870768_android/MapsActivity.java
+++ app/src/main/java/com/example/lab_simran_c0870768_android/MapsActivity.java
@@ -1,16 +1,37 @@
 package com.example.lab_simran_c0870768_android;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationRequest;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -18,12 +39,28 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.lab_simran_c0870768_android.databinding.ActivityMapsBinding;
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private FusedLocationProviderClient fusedLocationClient;
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
     Location mLastLocation;
     Marker mCurrLocationMarker;
+    private Geocoder geocoder;
+    private MarkerOptions markerOptions;
+    private static final int REQUEST_CODE = 1;
+    private FusedLocationProviderClient fusedClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,33 +72,171 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        geocoder = new Geocoder(this, Locale.getDefault());
+        fusedClient = LocationServices.getFusedLocationProviderClient(this);
+        Button mapTypeButton = findViewById(R.id.map_type_button);
+        mapTypeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the map object from the map fragment
+
+                // Check the current map type and change it to the opposite
+                if (mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL) {
+                    mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                } else {
+                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                }
+            }
+        });
+
     }
+
+    private GoogleMap.OnMapLongClickListener mMapLongClickListener = new GoogleMap.OnMapLongClickListener() {
+        @Override
+        public void onMapLongClick(LatLng latLng) {
+            stopUpdateLocation();
+            // Clearing all the markers from the Google Map
+            mMap.clear();
+
+            ArrayList<Marker> markers = new ArrayList<>();
+
+            Marker marker = mMap.addMarker(new MarkerOptions().position(latLng));
+            markers.add(marker);
+            // Adding a new marker to the Google Map
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLng);
+            markerOptions.title("Location");
+            mMap.addMarker(markerOptions);
+            Marker updatedMarker = markers.get(0);
+            // Reverse geocoding to get the address for the given LatLng
+            Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+            try {
+                List<Address> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+                if (addresses.size() > 0) {
+                    String address = addresses.get(0).getAddressLine(0);
+                    // Update the title of the marker
+                    updatedMarker.setTitle(address);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
-
-
+        if (!hasLocationPermission())
+            requestLocationPermission();
+        else
+            startUpdateLocation();
+        mMap.setOnMapLongClickListener(mMapLongClickListener);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(final Marker marker) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
+                builder.setTitle("Add to favorites");
+                builder.setMessage("Do you want to add this location to your favorites?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Add location to list of favorite places
+                        String locationName = marker.getTitle();
+                        LatLng latLng = marker.getPosition();
+                        Intent intent = new Intent();
+                        intent.putExtra("locationName", locationName);
+                        intent.putExtra("latLng", latLng);
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("No", null);
+                builder.show();
+                return true;
+            }
+        });
     }
-    //user location
-    public void onLocationChanged(Location location) {
 
-        mLastLocation = location;
-        if (mCurrLocationMarker != null) {
-            mCurrLocationMarker.remove();
+    private void startUpdateLocation() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                mMap.clear();
+                if (locationResult != null) {
+                    Location location = locationResult.getLastLocation();
+                    LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(userLocation).title("your location!"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
+                }
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
         }
-        //Place current location marker
-        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLng);
-        markerOptions.title("Current Position");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-        mCurrLocationMarker = mMap.addMarker(markerOptions);
+        fusedClient.requestLocationUpdates(locationRequest, locationCallback, null);
+    }
 
-        //move map camera
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(8));
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+    }
 
+    private boolean hasLocationPermission() {
+        return ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void stopUpdateLocation() {
+        if (fusedClient != null) {
+            fusedClient.removeLocationUpdates(locationCallback);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE) {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setMessage("The permission is mandatory")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+                            }
+                        }).create().show();
+            } else
+                startUpdateLocation();
+        }
+    }
+
+        public void searchLocation (View view){
+            EditText locationSearch = (EditText) findViewById(R.id.editText);
+            String location = locationSearch.getText().toString();
+            List<Address> addressList = null;
+
+            if (location != null || !location.equals("")) {
+                Geocoder geocoder = new Geocoder(this);
+                try {
+                    addressList = geocoder.getFromLocationName(location, 1);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Address address = addressList.get(0);
+                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                Toast.makeText(getApplicationContext(), address.getLatitude() + " " + address.getLongitude(), Toast.LENGTH_LONG).show();
+
+            }
+        }
 
     }
-}
+
+
